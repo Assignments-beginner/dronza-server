94aeda5fa7b8371d1ebc74422c9531b9fe882e77
avishekataur <avishekataur@gmail.com>
2021-11-10 11:06:28 +0600

    running-server

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cvpyv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//Main Async Function
+async function run() {
+  try {
+    await client.connect();
+    // console.log('Successfully Connected');
+    const database = client.db("drones");
+    // const productCollection = database.collection("products");
+    // const dummyCollection = database.collection("dummy");
+    const userCollection = database.collection("users");
+
+/*-------------------------------------------------------------------------------*\
+  //////////////////////////////// Users \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+\*-------------------------------------------------------------------------------*/
+
+    //POST API For Users
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      // console.log(result);
+      res.json(result);
+    });
+
+
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+//Test The Server Connection
+app.get("/", (req, res) => {
+  res.send("Running Drone Server.");
+});
+
+app.listen(port, () => {
+  console.log("Welcome to PORT", port);
+});
