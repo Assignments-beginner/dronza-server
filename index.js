2b54d913ea092385dc22ddead2005866d523cffa
avishekataur <avishekataur@gmail.com>
2022-01-25 08:00:14 +0600

    cors_w_simplified_one_two

--- index.js
+++ index.js
@@ -11,12 +11,15 @@ const port = process.env.PORT || 5000;
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 ///middleware
-app.use(
+
+/* app.use(
   cors({
     origin: "*",
     credentials: true,
   })
-);
+); */
+
+app.use(cors());
 app.use(express.json());
 
 //URI
